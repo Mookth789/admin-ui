9b053e2d287823a24343a775cd4f673fd73279bc
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-05 05:42:25 +0700

    Update menu.vue

--- src/views/system/menu/menu.vue
+++ src/views/system/menu/menu.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="n-layout-page-header">
-      <n-card :bordered="false" title="菜单权限管理">
-        页面数据为 Mock 示例数据，非真实数据。
+      <n-card :bordered="false" title="จัดการสิทธิ์">
+       ข้อมูลเพจเป็นข้อมูลตัวอย่างจำลอง ไม่ใช่ข้อมูลจริง
       </n-card>
     </div>
     <n-grid class="mt-4" cols="1 s:1 m:1 l:3 xl:3 2xl:3" responsive="screen" :x-gap="12">
@@ -12,7 +12,7 @@
             <n-space>
               <n-dropdown trigger="hover" @select="selectAddMenu" :options="addMenuOptions">
                 <n-button type="info" ghost icon-placement="right">
-                  添加菜单
+                  เพิ่มเมนู
                   <template #icon>
                     <div class="flex items-center">
                       <n-icon size="14">
@@ -23,7 +23,7 @@
                 </n-button>
               </n-dropdown>
               <n-button type="info" ghost icon-placement="left" @click="packHandle">
-                全部{{ expandedKeys.length ? '收起' : '展开' }}
+                ทั้งหมด{{ expandedKeys.length ? 'เอาออก' : 'ขยาย' }}
                 <template #icon>
                   <div class="flex items-center">
                     <n-icon size="14">
@@ -35,7 +35,7 @@
             </n-space>
           </template>
           <div class="w-full menu">
-            <n-input type="input" v-model:value="pattern" placeholder="输入菜单名称搜索">
+            <n-input type="input" v-model:value="pattern" placeholder="ใส่ชื่อเมนูเพื่อค้นหา">
               <template #suffix>
                 <n-icon size="18" class="cursor-pointer">
                   <SearchOutlined />
@@ -73,10 +73,10 @@
               <n-icon size="18">
                 <FormOutlined />
               </n-icon>
-              <span>编辑菜单{{ treeItemTitle ? `：${treeItemTitle}` : '' }}</span>
+              <span>แก้ไขเมนู{{ treeItemTitle ? `：${treeItemTitle}` : '' }}</span>
             </n-space>
           </template>
-          <n-alert type="info" closable> 从菜单列表选择一项后，进行编辑</n-alert>
+          <n-alert type="info" closable> หลังจากเลือกรายการจากเมนู，เพื่อแก้ไข</n-alert>
           <n-form
             :model="formParams"
             :rules="rules"
@@ -86,36 +86,36 @@
             v-if="isEditMenu"
             class="py-4"
           >
-            <n-form-item label="类型" path="type">
-              <span>{{ formParams.type === 1 ? '侧边栏菜单' : '' }}</span>
+            <n-form-item label="กรอก" path="type">
+              <span>{{ formParams.type === 1 ? 'เมนูแถบด้านล่าง' : '' }}</span>
             </n-form-item>
-            <n-form-item label="标题" path="label">
-              <n-input placeholder="请输入标题" v-model:value="formParams.label" />
+            <n-form-item label="ชื่อ" path="label">
+              <n-input placeholder="กรุณาใส่ชื่อเรื่อง" v-model:value="formParams.label" />
             </n-form-item>
-            <n-form-item label="副标题" path="subtitle">
-              <n-input placeholder="请输入副标题" v-model:value="formParams.subtitle" />
+            <n-form-item label="คำบรรยาย" path="subtitle">
+              <n-input placeholder="กรุณาใส่คำบรรยาย" v-model:value="formParams.subtitle" />
             </n-form-item>
-            <n-form-item label="路径" path="path">
-              <n-input placeholder="请输入路径" v-model:value="formParams.path" />
+            <n-form-item label="เส้นทาง" path="path">
+              <n-input placeholder="กรุณาใส่เส้นทาง" v-model:value="formParams.path" />
             </n-form-item>
-            <n-form-item label="打开方式" path="openType">
+            <n-form-item label="วิธีการเปิด" path="openType">
               <n-radio-group v-model:value="formParams.openType" name="openType">
                 <n-space>
-                  <n-radio :value="1">当前窗口</n-radio>
-                  <n-radio :value="2">新窗口</n-radio>
+                  <n-radio :value="1">หน้าต่างปัจจุบัน</n-radio>
+                  <n-radio :value="2">หน้าต่างใหม่</n-radio>
                 </n-space>
               </n-radio-group>
             </n-form-item>
-            <n-form-item label="菜单权限" path="auth">
-              <n-input placeholder="请输入权限，多个权限用，分割" v-model:value="formParams.auth" />
+            <n-form-item label="สิทธิ์เมนู" path="auth">
+              <n-input placeholder="โปรดป้อนการอนุญาติ，สิทธิ์หลายรายการ，การแบ่งส่วน" v-model:value="formParams.auth" />
             </n-form-item>
             <n-form-item path="auth" style="margin-left: 100px">
               <n-space>
                 <n-button type="primary" :loading="subLoading" @click="formSubmit"
-                  >保存修改</n-button
+                  >บันทึก</n-button
                 >
-                <n-button @click="handleReset">重置</n-button>
-                <n-button @click="handleDel">删除</n-button>
+                <n-button @click="handleReset">รีเซ็ต</n-button>
+                <n-button @click="handleDel">ลบ</n-button>
               </n-space>
             </n-form-item>
           </n-form>
@@ -136,12 +136,12 @@
   const rules = {
     label: {
       required: true,
-      message: '请输入标题',
+      message: 'กรุณาใส่ชื่อเรื่อง',
       trigger: 'blur',
     },
     path: {
       required: true,
-      message: '请输入路径',
+      message: 'กรุณาใส่เส้นทาง',
       trigger: 'blur',
     },
   };
@@ -170,12 +170,12 @@
 
   const addMenuOptions = ref([
     {
-      label: '添加顶级菜单',
+      label: 'เพิ่มเมนูด้านบน',
       key: 'home',
       disabled: false,
     },
     {
-      label: '添加子菜单',
+      label: 'เพิ่มเมนูย่อย',
       key: 'son',
       disabled: isAddSon,
     },
@@ -191,7 +191,7 @@
   });
 
   function selectAddMenu(key: string) {
-    drawerTitle.value = key === 'home' ? '添加顶栏菜单' : `添加子菜单：${treeItemTitle.value}`;
+    drawerTitle.value = key === 'home' ? 'เพิ่มแถบเมนูด้านบน' : `เพิ่มเมนูย่อย：${treeItemTitle.value}`;
     openCreateDrawer();
   }
 
@@ -216,15 +216,15 @@
 
   function handleDel() {
     dialog.info({
-      title: '提示',
-      content: `您确定想删除此权限吗?`,
-      positiveText: '确定',
-      negativeText: '取消',
+      title: 'แจ้งเตือน',
+      content: `คุณแน่ใจหรือไม่ ว่าต้องการลบการตั้งค่านี้`,
+      positiveText: 'ตกลง',
+      negativeText: 'ยกเลิก',
       onPositiveClick: () => {
-        message.success('删除成功');
+        message.success('ดำเนินการลบสำเร็จ');
       },
       onNegativeClick: () => {
-        message.error('已取消');
+        message.error('ยกเลิก');
       },
     });
   }
@@ -237,9 +237,9 @@
   function formSubmit() {
     formRef.value.validate((errors: boolean) => {
       if (!errors) {
-        message.error('抱歉，您没有该权限');
+        message.error('ขออภัย คุณไม่มีสิทธิ์ทำรายการนี้');
       } else {
-        message.error('请填写完整信息');
+        message.error('กรุณากรอกข้อมูลให้ครบถ้วน');
       }
     });
   }
