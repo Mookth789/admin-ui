5574f269ddee659293de1da596f58085f033c204
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-05 02:49:21 +0700

    Update index.vue

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -17,7 +17,7 @@
           :rules="rules"
         >
           <n-form-item path="username">
-            <n-input v-model:value="formInline.username" placeholder="กรุณากรอกชื่อผู้ใช้">
+            <n-input v-model:value="formInline.username" placeholder="请输入用户名">
               <template #prefix>
                 <n-icon size="18" color="#808695">
                   <PersonOutline />
@@ -30,7 +30,7 @@
               v-model:value="formInline.password"
               type="password"
               showPasswordOn="click"
-              placeholder="กรุณากรอกรหัสผ่าน"
+              placeholder="请输入密码"
             >
               <template #prefix>
                 <n-icon size="18" color="#808695">
@@ -42,27 +42,27 @@
           <n-form-item class="default-color">
             <div class="flex justify-between">
               <div class="flex-initial">
-                <n-checkbox v-model:checked="autoLogin">เข้าสู่ระบบอัตโนมัติ</n-checkbox>
+                <n-checkbox v-model:checked="autoLogin">自动登录</n-checkbox>
               </div>
               <div class="flex-initial order-last">
-                <a href="javascript:">ลืมรหัสผ่าน</a>
+                <a href="javascript:">忘记密码</a>
               </div>
             </div>
           </n-form-item>
           <n-form-item>
             <n-button type="primary" @click="handleSubmit" size="large" :loading="loading" block>
-              เข้าสู่ระบบ
+              登录
             </n-button>
           </n-form-item>
           <n-form-item class="default-color">
             <div class="flex view-account-other">
               <div class="flex-initial">
-                <span>วิธีการเข้าสู่ระบบอื่น ๆ</span>
+                <span>其它登录方式</span>
               </div>
               <div class="flex-initial mx-2">
                 <a href="javascript:">
                   <n-icon size="24" color="#2d8cf0">
-                    <LogoLine />
+                    <LogoGithub />
                   </n-icon>
                 </a>
               </div>
@@ -74,7 +74,7 @@
                 </a>
               </div>
               <div class="flex-initial" style="margin-left: auto">
-                <a href="javascript:">ลงทะเบียนบัญชี</a>
+                <a href="javascript:">注册账号</a>
               </div>
             </div>
           </n-form-item>
@@ -90,8 +90,7 @@
   import { useUserStore } from '@/store/modules/user';
   import { useMessage } from 'naive-ui';
   import { ResultEnum } from '@/enums/httpEnum';
-  import { PersonOutline, LockClosedOutline, LogoFacebook } from 'assets/facebook.png';
-  import { PersonOutline, LockClosedOutline, LogoLine } from 'assets/line.png';
+  import { PersonOutline, LockClosedOutline, LogoGithub, LogoFacebook } from '@vicons/ionicons5';
   import { PageEnum } from '@/enums/pageEnum';
   import { websiteConfig } from '@/config/website.config';
   interface FormState {
@@ -112,8 +111,8 @@
   });
 
   const rules = {
-    username: { required: true, message: 'กรุณาใส่ชื่อผู้ใช้', trigger: 'blur' },
-    password: { required: true, message: 'กรุณาใส่รหัสผ่าน', trigger: 'blur' },
+    username: { required: true, message: '请输入用户名', trigger: 'blur' },
+    password: { required: true, message: '请输入密码', trigger: 'blur' },
   };
 
   const userStore = useUserStore();
@@ -126,7 +125,7 @@
     formRef.value.validate(async (errors) => {
       if (!errors) {
         const { username, password } = formInline;
-        message.loading('เข้าสู่ระบบ...');
+        message.loading('登录中...');
         loading.value = true;
 
         const params: FormState = {
@@ -139,18 +138,18 @@
           message.destroyAll();
           if (code == ResultEnum.SUCCESS) {
             const toPath = decodeURIComponent((route.query?.redirect || '/') as string);
-            message.success('ล็อกอินสำเร็จกำลังจะเข้าระบบ');
+            message.success('登录成功，即将进入系统');
             if (route.name === LOGIN_NAME) {
               router.replace('/');
             } else router.replace(toPath);
           } else {
-            message.info(msg || 'การเข้าสู่ระบบล้มเหลว');
+            message.info(msg || '登录失败');
           }
         } finally {
           loading.value = false;
         }
       } else {
-        message.error('กรุณากรอกข้อมูลให้ครบถ้วนและตรวจสอบรหัสยืนยัน');
+        message.error('请填写完整信息，并且进行验证码校验');
       }
     });
   };
