c0e711eae5c69d34deed0bb641274deec371741d
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-07 23:41:54 +0700

    Update animateSetting.ts

--- src/settings/animateSetting.ts
+++ src/settings/animateSetting.ts
@@ -1,8 +1,8 @@
 export const animates = [
-  { value: 'zoom-fade', label: '渐变' },
-  { value: 'zoom-out', label: '闪现' },
-  { value: 'fade-slide', label: '滑动' },
-  { value: 'fade', label: '消退' },
-  { value: 'fade-bottom', label: '底部消退' },
-  { value: 'fade-scale', label: '缩放消退' },
+  { value: 'zoom-fade', label: 'การไล่ระดับสี' },
+  { value: 'zoom-out', label: 'แฟลช' },
+  { value: 'fade-slide', label: 'สไลด์' },
+  { value: 'fade', label: 'บรรเทาลง' },
+  { value: 'fade-bottom', label: 'ด้านล่างลดลง' },
+  { value: 'fade-scale', label: 'ซูมจาง' },
 ];
